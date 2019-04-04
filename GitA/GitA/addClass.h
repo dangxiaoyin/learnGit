7343c200b6aa1c32956b8f71671b30882f4b1e62
dangxy <945979125@qq.com>
2019-04-04 14:29:47 +0800

    提交addClass

---
+++ GitA/GitA/addClass.h
@@ -0,0 +1,17 @@
+//
+//  addClass.h
+//  GitA
+//
+//  Created by Apple on 2019/4/4.
+//  Copyright © 2019 ht. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface addClass : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GitA/GitA/addClass.m
@@ -0,0 +1,17 @@
+//
+//  addClass.m
+//  GitA
+//
+//  Created by Apple on 2019/4/4.
+//  Copyright © 2019 ht. All rights reserved.
+//
+
+#import "addClass.h"
+
+@implementation addClass
+
+- (void)initclass {
+    
+}
+
+@end
