50239e4712d4d40a69057e66e4df13d85c32a4de
dangxy <945979125@qq.com>
2019-04-04 13:23:09 +0800

    提交TabbarViewController

---
+++ GitA/GitA/TabBarViewController.h
@@ -0,0 +1,17 @@
+//
+//  TabBarViewController.h
+//  GitA
+//
+//  Created by Apple on 2019/4/4.
+//  Copyright © 2019 ht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TabBarViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GitA/GitA/TabBarViewController.m
@@ -0,0 +1,32 @@
+//
+//  TabBarViewController.m
+//  GitA
+//
+//  Created by Apple on 2019/4/4.
+//  Copyright © 2019 ht. All rights reserved.
+//
+
+#import "TabBarViewController.h"
+
+@interface TabBarViewController ()
+
+@end
+
+@implementation TabBarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
