bab4bc2de3ad0ceae0400c716745d0d4e9ed2781
dangxy <945979125@qq.com>
2019-04-04 13:57:27 +0800

    提交ThisModel

---
+++ GitA/GitA/ThisModel.h
@@ -0,0 +1,17 @@
+//
+//  ThisModel.h
+//  GitA
+//
+//  Created by Apple on 2019/4/4.
+//  Copyright © 2019 ht. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ThisModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
